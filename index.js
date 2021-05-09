5358b851d9980708934c1ab8f237f7743b377a03
Sanjay Shivraj <shivraj.sanjay@gmail.com>
2021-05-09 01:16:40 -0400

    project init

---
+++ index.js
@@ -0,0 +1,7 @@
+require('dotenv').config();
+const Discord = require('discord.js');
+const bot = new Discord.Client();
+
+const TOKEN = process.env.TOKEN;    //We'll get one of these soon
+
+bot.login(TOKEN);
