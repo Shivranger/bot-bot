ee4c9ff1ec91d2e6053a9e39783746d0baf0bd20
Sanjay Shivraj <shivraj.sanjay@gmail.com>
2021-05-09 14:06:58 -0400

    ping-pong working

--- index.js
+++ index.js
@@ -5,3 +5,13 @@ const bot = new Discord.Client();
 const TOKEN = process.env.DISCORD_BOT_TOKEN;    //We'll get one of these soon
 
 bot.login(TOKEN);
+
+bot.on('ready', () => {
+    console.info(`Logged in as ${bot.user.tag}!`);
+});
+
+bot.on('message', msg => {
+    if (msg.content === 'ping') {
+      msg.reply('pong');
+    }
+});
