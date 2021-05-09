1d36e1787345540c0f1926d7348b62952aea2b8f
Curtis Ruan <thecurtisruan@gmail.com>
2021-05-09 12:52:58 -0400

    Added Token

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ require('dotenv').config();
 const Discord = require('discord.js');
 const bot = new Discord.Client();
 
-const TOKEN = process.env.TOKEN;    //We'll get one of these soon
+//Discord Token created through Discord.com/developers/Applciations
+const TOKEN = 'a45c47ca2d1a98fa0e7f2ccb69d632dfa8e213094b1345693925022066548145' //process.env.TOKEN;    //We'll get one of these soon
 
 bot.login(TOKEN);
