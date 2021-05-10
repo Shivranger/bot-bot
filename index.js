62de90178448ee014f52a3ac959d0dcf6a367307
Curtis Ruan <thecurtisruan@gmail.com>
2021-05-10 19:31:31 -0400

    Set up Command  Parser and Command Folder

--- index.js
+++ index.js
@@ -1,23 +1,62 @@
 require('dotenv').config();
+
+const TOKEN = process.env.DISCORD_BOT_TOKEN; 
 const Discord = require('discord.js');
 const bot = new Discord.Client();
+const fs = require('fs');
+const commandPrefix = '!'
 
-const TOKEN = process.env.DISCORD_BOT_TOKEN;    //We'll get one of these soon
+
+bot.commands = new Discord.Collection();
 
 bot.login(TOKEN);
 
 bot.on('ready', () => {
     console.info(`Logged in as ${bot.user.tag}!`);
+    fs.readdir('./commands', (err, files) =>{
+      if(err) {
+        console.log(err);
+      }
+      //<command>.js
+      //<command>
+      let jsfile = files.filter(f => f.split('.').pop() == 'js');
+      
+      if(jsfile.length === 0) {
+        return console.log('Could not find any commands')
+      }
+
+      jsfile.forEach(f => {
+        let props = require(`./commands/${f}`)
+        bot.commands.set(props.help.name, props);
+      })
+
+    })
+
 });
 
 bot.on('message', msg => {
 
   let sender = msg.author
+  if(sender.bot){
+    return;
+  }
   let message = msg.content.toUpperCase();
+  let MessageArray = message.split(' ');
+  let cmd = MessageArray[0].slice(1)
+  let args = MessageArray.slice(1);
 
-  let prefix = '>'
+  if(!msg.content.startsWith(commandPrefix)){
+    return;
+  }
 
-  if(message === prefix + 'PING') {
+  let commandfile = bot.commands.get(cmd);
+  if(commandfile){
+    commandfile.run(bot,msg,args);
+  }else{
+    msg.channel.send('I cannot do that.');
+  }
+
+  if(message === commandPrefix + 'PING') {
     msg.channel.send('Pong!')
   }
 
