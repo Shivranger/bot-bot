e2c13e5f2d146f29376702bb1552048700b45a6a
Sanjay Shivraj <shivraj.sanjay@gmail.com>
2021-07-02 21:54:00 -0400

    skeleton code and folder

---
+++ commands/movie-commands.js
@@ -0,0 +1,14 @@
+exports.run = async(bot, message, args, sender) => {
+  switch (args[0]){
+    case 'ADD':
+      //add movies
+      break;
+    case 'RANDOM':
+      //get a random movie 
+      break;
+  }
+}
+
+exports.help = {
+    name: 'MOVIE'
+}
